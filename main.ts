00a60dba20d43348ca951a444091d68f02c8e869
Janae Stewart <70561233+janaestewart@users.noreply.github.com>
2021-05-06 15:02:48 -0400

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,37 @@
+// so the spaceShip wont fly off screen
+// this is so the projectile will spawn on our spaceship sprite when we hit the A button
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    projectile = sprites.createProjectileFromSprite(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . 1 . . . . 1 . . . . . . 
+        . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
+        . . 1 . . . 1 . . . . . 1 . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, spaceShip, 200, 0)
+})
+statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
+    status.spriteAttachedTo().destroy()
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -25
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(-1)
+})
 /**
  * enemyShip = sprites.create(img`
  * 
@@ -35,43 +69,9 @@
  * 
  * `, SpriteKind.Enemy)
  */
-// so the spaceShip wont fly off screen
-// this is so the projectile will spawn on our spaceship sprite when we hit the A button
-controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    projectile = sprites.createProjectileFromSprite(img`
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . 1 . . . . 1 . . . . . . 
-        . . 9 9 9 9 9 9 9 9 9 9 9 9 . . 
-        . . 1 . . . 1 . . . . . 1 . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        . . . . . . . . . . . . . . . . 
-        `, spaceShip, 200, 0)
-})
-statusbars.onZero(StatusBarKind.EnemyHealth, function (status) {
-    status.spriteAttachedTo().destroy()
-})
-sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
-    sprite.destroy()
-    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -25
-    info.changeScoreBy(1)
-})
-sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    otherSprite.destroy()
-    info.changeLifeBy(-1)
-})
 let statusbar: StatusBarSprite = null
-let enemyShip: Sprite = null
 let projectile: Sprite = null
+let enemyShip: Sprite = null
 let spaceShip: Sprite = null
 spaceShip = sprites.create(img`
     . . . . . . . . . . . . . . . . 
@@ -91,10 +91,29 @@ spaceShip = sprites.create(img`
     . . 2 2 2 . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-info.setLife(3)
+// info.setLife(3)
 controller.moveSprite(spaceShip)
 // so the spaceShip wont fly off screen
 spaceShip.setFlag(SpriteFlag.StayInScreen, true)
+let projectile2 = sprites.createProjectileFromSprite(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 3 3 . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, enemyShip, 10, 10)
+statusbars.getStatusBarAttachedTo(0, null)
 // continously spawn enemies every half second
 game.onUpdateInterval(1000, function () {
     enemyShip = sprites.create(img`
